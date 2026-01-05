1160ed5069f48808c5271a376200515ebbf8e852
aditya-biju <adityabiju06@gmail.com>
2026-01-05 18:14:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,22 @@
 /* LUMOS INTRO */
 alert("Magic Loaded ✨");
 
-setTimeout(() => {
-  document.getElementById("lumos").style.display = "none";
-  document.getElementById("content").style.display = "block";
-}, 3500);
+console.log("Magic Loaded");
 
-/* FLOATING MAGIC */
+window.onload = () => {
+  setTimeout(() => {
+    document.getElementById("lumos").style.display = "none";
+    document.getElementById("content").style.display = "block";
+  }, 3000);
+};
+
 setInterval(() => {
   const p = document.createElement("div");
   p.className = "particle";
-  p.style.left = Math.random() * 100 + "vw";
-  p.style.animationDuration = 2 + Math.random() * 2 + "s";
+  p.style.left = Math.random() * window.innerWidth + "px";
+  p.style.top = window.innerHeight + "px";
   document.body.appendChild(p);
 
-  setTimeout(() => p.remove(), 4000);
-}, 120);
-
-/* SPELL EFFECT */
-function castSpell() {
-  document.body.style.background = "white";
-
-  setTimeout(() => {
-    document.body.style.background = "";
-  }, 150);
-
-  for (let i = 0; i < 40; i++) {
-    const p = document.createElement("div");
-    p.className = "particle";
-    p.style.left = 50 + Math.random() * 10 - 5 + "vw";
-    document.body.appendChild(p);
-
-    setTimeout(() => p.remove(), 3000);
-  }
-}
+  setTimeout(() => p.remove(), 3000);
+}, 100);
 
